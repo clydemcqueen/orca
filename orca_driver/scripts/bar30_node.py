1a3120d4e2d73034a3629eb9312c6ecc1d3755e5
Clyde McQueen <clyde@mcqueen.net>
2017-10-27 03:28:57 +0000

    Bar30 node: add dep on python-smbus; change publish rate to 10Hz

--- orca_driver/scripts/bar30_node.py
+++ orca_driver/scripts/bar30_node.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-import ms5837
+# This is a simple ROS wrapper for the BlueRobotics Bar30 sensor
+# https://github.com/bluerobotics/ms5837-python/blob/master/ms5837.py must be in the Python path
+
 import rospy
+import ms5837
 from orca_msgs.msg import Barometer
   
 def run():
@@ -10,15 +13,17 @@ def run():
   pub = rospy.Publisher('/barometer', Barometer, queue_size=1)
 
   # Connect to the Bar30 sensor on I2C bus 1
+  rospy.loginfo("Connecting to Bar30...")
   sensor = ms5837.MS5837_30BA(1)
   if not sensor.init():
     rospy.logerr("Can't initialize Bar30")
     exit(1)
 
   sensor.setFluidDensity(ms5837.DENSITY_SALTWATER)
-  rate = rospy.Rate(50)
+  rate = rospy.Rate(10) # Max rate on the RPi3 seems to be 22 hz
 
   # Loop until Ctrl-C
+  rospy.loginfo("Connected to Bar30, entering main loop")
   while not rospy.is_shutdown():
     if sensor.read():
       msg = Barometer()
