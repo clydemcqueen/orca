f5bd21ea661ed2bd8a720ac5f8bfcc2373d419f1
Clyde McQueen <clyde@mcqueen.net>
2018-02-16 08:57:46 -0800

    Refactored driver and base launch; makes it easier to run the base nodes on the sub or topside

--- orca_driver/scripts/start_orca_driver.sh
+++ orca_driver/scripts/start_orca_driver.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Start orca ROS nodes
+# Start orca ROS driver
 
-# To install:           sudo cp orca_ros.service /lib/systemd/system
-# To start:             sudo systemctl start orca_ros.service
-# To stop:              sudo systemctl stop orca_ros.service
-# To start on boot:     sudo systemctl enable orca_ros.service
-# To not start on boot: sudo systemctl disable orca_ros.service
+# To install:           sudo cp orca_driver.service /lib/systemd/system
+# To start:             sudo systemctl start orca_driver.service
+# To stop:              sudo systemctl stop orca_driver.service
+# To start on boot:     sudo systemctl enable orca_driver.service
+# To not start on boot: sudo systemctl disable orca_driver.service
 
-bash -c "source /home/pi/orca_catkin_ws/devel/setup.bash && stdbuf -o L roslaunch orca_driver sub.launch"
+bash -c "source /home/pi/orca_catkin_ws/devel/setup.bash && stdbuf -o L roslaunch orca_driver driver.launch"
